c5178be03092144a12c72ef2b455b07580433272
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2024-01-09 07:38:57 +0000

    - Tidied up Factory.

--- 02-factory/ShapeFactory.py
+++ 02-factory/ShapeFactory.py
@@ -1,16 +1,14 @@
-from Shape import Shape
-from Circle import Circle
-from Square import Square
+from Circle    import Circle
+from Square    import Square
 from Rectangle import Rectangle
 
-class ShapeFactory(Shape):
+class ShapeFactory:
     _dispatch = {
         'CIRCLE': Circle(),
         'SQUARE': Square(),
         'RECTANGLE': Rectangle()
     }
 
-    def get_shape(type):
-        if type in ShapeFactory._dispatch:
-            return ShapeFactory._dispatch[type]
-            #super(Shape, cls).__new__(cls)
+    def get_shape(self, type):
+        if type in self._dispatch:
+            return self._dispatch[type]
--- 02-factory/test_factory.py
+++ 02-factory/test_factory.py
@@ -2,14 +2,18 @@ import pytest
 from pytest import approx
 from ShapeFactory import ShapeFactory
 
-def test_circle_area():
-    circle = ShapeFactory.get_shape("CIRCLE")
+@pytest.fixture()
+def shape_factory():
+    return ShapeFactory()
+
+def test_circle_area(shape_factory):
+    circle = shape_factory.get_shape("CIRCLE")
     assert approx(circle.area(10)) == 314
 
-def test_square_area():
-    square = ShapeFactory.get_shape("SQUARE")
+def test_square_area(shape_factory):
+    square = shape_factory.get_shape("SQUARE")
     assert approx(square.area(3)) == 9
 
-def test_rectangle_area():
-    rectangle = ShapeFactory.get_shape("RECTANGLE")
+def test_rectangle_area(shape_factory):
+    rectangle = shape_factory.get_shape("RECTANGLE")
     assert approx(rectangle.area(2, 4)) == 8
